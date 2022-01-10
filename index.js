6b38d36f86c5e372a76610b6b027292bd899c895
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-01-10 17:43:21 +0000

    code for lab 6

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const public = path.join(__dirname,'public');
+app.use(express.static(public));
+
+const router = require('./routes/guestbookRoutes');
+app.use('/', router); 
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000. Ctrl^c to quit.');
+})
