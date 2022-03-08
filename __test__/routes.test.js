1ccd01e7f5c6640247d35f7cf6aa6060a775336b
KEVINURAMA <kurama200@caledonian.ac.uk>
2022-03-08 15:02:25 +0000

    changes

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -10,3 +10,17 @@ test('responds to /new', () => {
     controller.new_entry(req, res);
     expect(res.text).toEqual('<h1>Not yet implemented: show a new entry page.</h1>');
 })
+
+test('landing page renders', () => {
+    const req = {}
+    const res = {render: jest.fn()}
+    controller.landing_page(req, res)
+    expect(res.sender.mock.calls.length).toBe(1);
+    expect(res.render.mock.calls[0][0]).toBe('entries');
+})
+
+
+
+
+
+
